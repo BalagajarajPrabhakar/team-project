1574149800cd4bd3db2172ab0f176f32c8548987
111522203058 <146010359+111522203058@users.noreply.github.com>
2026-02-27 22:18:08 +0530

    reg form

---
+++ registration.py
@@ -0,0 +1,17 @@
+print("===== User Registration Form =====")
+
+# Taking input from user
+name = input("Enter your Name: ")
+email = input("Enter your Email: ")
+username = input("Enter your Username: ")
+password = input("Enter your Password: ")
+phone = input("Enter your Phone Number: ")
+
+print("\n===== Registration Successful! =====")
+print("Here are your registered details:\n")
+
+print("Name:", name)
+print("Email:", email)
+print("Username:", username)
+print("Password:", password)
+print("Phone Number:", phone)
