022af41ad4e7cb2a586dc2664d6989897a197fca
Balagajaraj Prabhakar <93154220+BalagajarajPrabhakar@users.noreply.github.com>
2026-02-27 11:17:42 +0530

    Update and rename index.html to app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+# Simple User Registration Program (Basic Python Only)
+
+print("===== User Registration =====")
+
+# Get user details
+name = input("Enter your full name: )
+email = input("Enter your email: ")
+username = input("Create a username: ")
+password = input"Create a password: ")
+phone = input("Enter your phone number: ")
+
+# Display confirmation
+print("\n===== Registration Successful =====")
+print Registered Details:")
+print("Name      :", name)
+print("Email     :", email)
+prin("Username  :", username)
+print("Phone     :" phone)
